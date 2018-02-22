ef340983983b4798c72e8d3b71da6c1dd18be7f5
Hennessy <hennykdev@gmail.com>
2018-02-22 08:36:10 -0600

    added initial proj

---
+++ CatGen/CatGen.h
@@ -0,0 +1,19 @@
+//
+//  CatGen.h
+//  CatGen
+//
+//  Created by Emily Hennessy on 2/22/18.
+//  Copyright © 2018 hennykat. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for CatGen.
+FOUNDATION_EXPORT double CatGenVersionNumber;
+
+//! Project version string for CatGen.
+FOUNDATION_EXPORT const unsigned char CatGenVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CatGen/PublicHeader.h>
+
+
